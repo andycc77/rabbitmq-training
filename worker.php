cc2b853c54c5242fcf15f2baa62bc95cb7f30637
andycc77 <andycc77e@gmail.com>
2022-10-18 00:37:02 +0800

    worker queues

---
+++ worker.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [x] Done", "\n";
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+};
+
+$channel->basic_qos(null, 1, null);//在同一時刻，不要發送超過一條消息給一個worker，直到他已經處理了上一條消息並且做出了響應。
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
+
+?>
